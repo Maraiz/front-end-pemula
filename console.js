b4b3b1557d26442249cdca34464a27954c0fe4a5
Maraiz <Anakbudiman01@gmail.com>
2025-03-13 12:37:19 +0700

    awalan

---
+++ console.js
@@ -0,0 +1,12 @@
+// console.log('ini adalah console log');
+// console.info('ini adalah console info');
+// console.warn('ini adalah console warm');
+// console.error('ini adalah console eror');
+
+function alert (nama) {
+    console.log('hati hati ' + nama);
+}
+
+alert('Maulana')
+
+window.alert ('Maulana');
---
+++ index.js
@@ -0,0 +1,22 @@
+// const tamu = prompt('Siapa Namamu?');
+// alert("Namaku adalah " + tamu + "!");
+//  const pesan = ('Selamat datang, user');
+//  alert(pesan);
+
+//  const test = prompt('Siapa Namamu');
+// console.log(test);
+
+//  let pesan = prompt('type of test');
+//  console.log(`Tipe data dari ${pesan} adalah ${typeof pesan} `);
+ 
+// let contoh = Number(prompt('masukan angka'));
+// console.log(typeof contoh);
+
+
+// let pesanInput = prompt('Silakan masukkan angka yang Anda suka!');
+// let pesanInputConvertedToNumber = Number(pesanInput);
+// console.log(`Tipe data dari \`pesanInputConvertedToNumber\` adalah ${typeof pesanInputConvertedToNumber}`);
+
+
+let contoh = prompt('Namaku adalah', 'Maulana');
+console.log(contoh);
